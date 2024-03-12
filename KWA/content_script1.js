6debc71eba909ac387ccac734c1234a8e406d8dd
MrScytheMan <mrscytheman69@gmail.com>
2024-03-13 00:26:16 +0100

    change code source to fork

--- KWA/content_script1.js
+++ KWA/content_script1.js
@@ -4,13 +4,13 @@
      scr.innerHTML = `const LoadScript = (url, cb) => { fetch(url).then(res => res.json()).then((out) => { cb(out) }).catch(err => { throw err }); }
  
  const links = {
-    "link0":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/mains/charactersManager.js",
-    "link1":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/mains/ballExp.js",
-    "link2":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/mains/ballUpgrade.js",
-    "link3":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/mains/ballReset.js",
-    "link4":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/mains/ballManager.js",
-    "link5":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/mains/script1-2.js",
-    "link6":"https://raw.githubusercontent.com/KWSforAll/KWSforAll/mains/ekwipunek.js",
+    "link0":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/charactersManager.js",
+    "link1":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ballExp.js",
+    "link2":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ballUpgrade.js",
+    "link3":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ballReset.js",
+    "link4":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ballManager.js",
+    "link5":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/script1-2.js",
+    "link6":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ekwipunek.js",
  };
  
  const fetchPromises = Object.values(links).map(link => {
