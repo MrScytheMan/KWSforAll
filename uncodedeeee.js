282f2fb6c8e1b00e999d663f878f321197ac1b91
MrScytheMan <mrscytheman69@gmail.com>
2024-05-03 10:39:42 +0200

    lower cd for pvm

--- uncodedeeee.js
+++ uncodedeeee.js
@@ -1603,7 +1603,7 @@ if (typeof GAME === 'undefined') {} else {
                 return res;
             };
             var RESP = {
-                wait: 60,
+                wait: 1,
                 stop: true,
                 checkOST: true,
                 checkSSJ: true,
