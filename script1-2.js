2657862b90909bb6b8d4c1428872a862d1911528
KWS4ALL <kwsforall@proton.me>
2024-05-08 22:58:14 +0200

    Bump version

--- script1-2.js
+++ script1-2.js
@@ -2387,7 +2387,7 @@ if (typeof GAME === 'undefined') { } else {
         let roll2 = false;
         let roll1 = false;
         let roll3 = false;
-        let version = '3.4.4';
+        let version = '3.4.5';
     }
     )
 }
