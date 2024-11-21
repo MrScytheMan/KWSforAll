4ef449ff1b3a626fca4b6d9b573ee6619d3e76af
Yasoen98 <smolamk@gmail.com>
2024-11-21 13:12:20 +0000

    Update script1-2.js

--- script1-2.js
+++ script1-2.js
@@ -1097,7 +1097,7 @@ if (typeof GAME === 'undefined') { } else {
                         isAutoBlessActive = false;
                         console.log("Automatyczne błogosławienie zostało wyłączone.");
                       } else {
-                        blessInterval = setInterval(this.autobless(), 15000);
+                        blessInterval = setInterval(autobless(), 15000);
                         isAutoBlessActive = true;
                         console.log("Automatyczne błogosławienie zostało włączone.");
                       }
