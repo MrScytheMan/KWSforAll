04bbfba38614ffba148532aa6b3de390e59cb18c
Yasoen98 <smolamk@gmail.com>
2024-11-22 20:03:44 +0000

    Update script1-2.js

--- script1-2.js
+++ script1-2.js
@@ -1177,8 +1177,7 @@ if (typeof GAME === 'undefined') { } else {
                         #bonusMenu {display: none; position: absolute; top: 80px; right: 5px; padding: 10px; background: rgba(48, 49, 49, 0.8); border: solid #ffffff7a 1px; border-radius: 5px; z-index: 10;}
                         #bonusMenu div {color: #ffffff; font-size: 16px; font-weight: bold; margin-bottom: 10px; text-align: center; }
                         #bonusMenu select {margin: 5px 0; background: #ffffff99; border: solid #6f6f6f 1px; border-radius: 5px; color: black; display: block; width: 100%;}
-                        #startButton {display: block; margin: 20px auto; color: white; background-color: #6f6f6f; border: none; border-radius: 8px; font-size: 18px; padding: 10px 20px; cursor: pointer; text-align: center;}
-                        #startButton:hover {background-color: #ffffff99; color: black;}`;
+                        .startButton {display: block; margin: 20px auto;}`;
                         let petHTML = `<div id="bonusMenu"><div><b>Wybierz bonusy:</b></div> ${this.generateBonusSelects(4)} <div><b>Wybierz ID Peta:</b></div><select id="petIdSelect">${this.generatePetOptions()}</select> <button class="newBtn startButton">Start</button></div>`;
                         let isPetBonchActive = false;
                         let petInterval = null;
@@ -1208,7 +1207,11 @@ if (typeof GAME === 'undefined') { } else {
                     });
                     $("body").on("click", '.startButton', function () {
                         console.log("test passed")
-                       /* isPetBonchActive = true;
+                        /*if(isPetBonchActive){
+                            isPetBonchActive = false;
+                        } else {
+                            isPetBonchActive = true;
+                        }
                         const selectedOptions = Array.from($('#bonusMenu select').not('#petIdSelect'))
                         .map(select => {
                           const value = select.value;
