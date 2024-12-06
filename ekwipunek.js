249ca902207c4d1bf7ce165e88317eefe5db51ed
Yasoen98 <152070840+Yasoen98@users.noreply.github.com>
2024-12-06 20:08:06 +0100

    Update ekwipunek.js

--- ekwipunek.js
+++ ekwipunek.js
@@ -386,7 +386,7 @@ class filterQuest {
                 };
                 
                 // Dodanie HTML i CSS za pomocą jQuery
-                $('#map_y').after(questFilterHTML);
+                $('#rightArrow').after(questFilterHTML);
                 $("#quest-filter-input").css(questFilterCSS);
                 
                 // Dodanie nasłuchiwacza na pole tekstowe
