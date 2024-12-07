8344a7d63bc4096a9458a64221034358c672b974
Yasoen98 <152070840+Yasoen98@users.noreply.github.com>
2024-12-07 13:37:44 +0000

    Update script1-2.js

--- script1-2.js
+++ script1-2.js
@@ -102,9 +102,11 @@ if (typeof GAME === 'undefined') { } else {
                 }, 1000);
                 this.setWebsiteBackground();
                 this.bindClickHandlers();
-                GAME.socket.on('gr', (res) => {
+                const socketID = Object.keys(GAME).filter(key => key.startsWith('xxx') && key.endsWith('Order'))[0];
+                GAME[socketID]((res) => { this.handleSockets(res); });
+                /*GAME.socket.on('gr', (res) => {
                     this.handleSockets(res);
-                });
+                });*/
             }
             isLogged(cb) {
                 let waitForID = setInterval(() => {
