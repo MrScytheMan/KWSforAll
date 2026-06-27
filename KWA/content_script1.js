9d2e082f7826f4e73451e88504d5c62646c3952e
MrScytheMan <mrscytheman69@gmail.com>
2026-06-27 13:36:36 +0200

    extension cleanup

--- KWA/content_script1.js
+++ KWA/content_script1.js
@@ -1,33 +1,20 @@
- window.onload = function() {
-    var scr = document.createElement("script");
-     scr.type="text/javascript";
-     scr.innerHTML = `const LoadScript = (url, cb) => { fetch(url).then(res => res.json()).then((out) => { cb(out) }).catch(err => { throw err }); }
- 
- const links = {
-    "link1":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ballExp.js",
-    "link2":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ballUpgrade.js",
-    "link3":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ballReset.js",
-    "link4":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ballManager.js",
-    "link5":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/script1-2.js",
-    "link6":"https://raw.githubusercontent.com/MrScytheMan/KWSforAll/main/ekwipunek.js",
- };
- 
- const fetchPromises = Object.values(links).map(link => {
-   return fetch(link).then(response => response.text());
- });
- 
- Promise.all(fetchPromises)
-   .then(scripts => {
-     scripts.forEach(script => {
-       $("body").append("<script>"+script+"</script>");
-     });
-   })
-   .catch(err => { 
-     console.log("Error fetching data:", err); 
-   });
- 
- 
- 
- `    
-   document.body.appendChild(scr) 
- }
+window.onload = function() {
+    const remoteScripts = [
+        "https://mrscytheman.github.io/KWSforAll/ballExp.js",
+        "https://mrscytheman.github.io/KWSforAll/ballUpgrade.js",
+        "https://mrscytheman.github.io/KWSforAll/ballReset.js",
+        "https://mrscytheman.github.io/KWSforAll/ballManager.js",
+        "https://mrscytheman.github.io/KWSforAll/ekwipunek.js",
+        "https://mrscytheman.github.io/KWSforAll/script1-2.js"
+    ];
+
+    remoteScripts.forEach(url => {
+        const script = document.createElement('script');
+        script.src = url;
+        script.type = "text/javascript";
+        script.onload = function() {
+            this.remove();
+        };
+        document.body.appendChild(script);
+    });
+};
